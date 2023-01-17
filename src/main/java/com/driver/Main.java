109fd3fd8d21a0c4ef5e6a6d5fb865da57444d03
Sushant <sushantkadam936@gmail.com>
2023-01-17 11:34:50 +0530

    end

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,12 +7,14 @@ public class Main {
             return x+y;
         }
 
-        public int product(int x, int y, int z) {
-            return  x+y+z;
-        }
+
 
         public double product(double x, double y) {
             return x+y;
+        }
+
+        public int product(int x, int y, int z) {
+            return  x+y+z;
         }
     }
 
