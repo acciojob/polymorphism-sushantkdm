fae7872a3965566266ff96e4c79c1954a85f835d
Sushant <sushantkadam936@gmail.com>
2023-01-18 14:14:12 +0530

    pol

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,17 +4,17 @@ public class Main {
 
     public static class Product{
         public int product(int x, int y) {
-            return x+y;
+            return x*y;
         }
 
 
 
         public double product(double x, double y) {
-            return x+y;
+            return x*y;
         }
 
         public int product(int x, int y, int z) {
-            return  x+y+z;
+            return  x*y*z;
         }
     }
 
